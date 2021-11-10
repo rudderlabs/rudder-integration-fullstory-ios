2a7b8d19cb0b44c5d9784211457b55f2d49894dc
1abhishekpandey <abhishekpandey10196@gmail.com>
2021-11-10 17:09:15 +0530

    Improved Integration class

--- Rudder-FullStory/Classes/RudderFullStoryIntegration.m
+++ Rudder-FullStory/Classes/RudderFullStoryIntegration.m
@@ -12,10 +12,6 @@
 
 - (instancetype)initWithConfig:(NSDictionary *)config withAnalytics:(RSClient *)client {
     self = [super init];
-    if (self)
-    {
-        
-    }
     return self;
 }
 
@@ -23,17 +19,20 @@
     NSString *type = message.type;
     if ([type isEqualToString:@"identify"])
     {
-        NSString *userID = !isEmpty(message.userId) ? message.userId : message.anonymousId;
         NSMutableDictionary<NSString*, NSObject*>* traits = [message.context.traits mutableCopy];
-        if (traits != nil) {
+        if (!isEmpty(traits)) {
             [traits removeObjectForKey:@"userId"];
         }
-        [FS identify:userID userVars:traits];
+        if (!isEmpty(message.userId)) {
+            [FS identify:message.userId userVars:traits];
+            return;
+        }
+        [FS setUserVars:traits];
     }
     else if ([type isEqualToString:@"track"])
     {
         if (!isEmpty(message.event)) {
-            [FS event:[self getTrimKey:[@"screen view " stringByAppendingString:message.event]] properties:message.properties];
+            [FS event:[self getTrimKey:message.event] properties:[self getSuffixProperty:message.properties]];
             return;
         }
         [RSLogger logDebug:@"Event name is not present in the Track call. Hence, event not sent"];
@@ -41,7 +40,12 @@
     else if ([type isEqualToString:@"screen"])
     {
         if (!isEmpty(message.event)) {
-            [FS event:[self getTrimKey:[@"screen view " stringByAppendingString:message.event]] properties:message.properties];
+            NSMutableDictionary *screenProperties = [self getSuffixProperty:message.properties];
+            if (isEmpty(screenProperties)) {
+                screenProperties = [[NSMutableDictionary alloc] init];
+            }
+            [screenProperties setObject:message.event forKey:@"name"];
+            [FS event:@"Screen Viewed" properties:screenProperties];
             return;
         }
         [RSLogger logDebug:@"Event name is not present in the Screen call. Hence, event not sent"];
@@ -51,7 +55,6 @@
         [RSLogger logDebug:@"FullStory Integration: Message type not supported"];
     }
 }
-
 
 - (void)dump:(nonnull RSMessage *)message {
     @try
@@ -67,6 +70,45 @@
     {
         [RSLogger logError:[[NSString alloc] initWithFormat:@"%@", ex]];
     }
+}
+
+- (NSMutableDictionary *)getSuffixProperty:(NSDictionary *)properties {
+    if (isEmpty(properties)){
+        return nil;
+    }
+    NSMutableDictionary *suffixedProperty = [[NSMutableDictionary alloc] init];
+    for (NSString *propertyKey in properties) {
+        NSObject *value = properties[propertyKey];
+        NSString *key = [[propertyKey stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] stringByReplacingOccurrencesOfString:@" " withString:@"_"];
+        // default to no suffix;
+        NSString *suffix = @"";
+        if ([value isKindOfClass:[NSNumber class]]) {
+            // defaut to int
+            suffix = @"_int";
+            NSNumber *n = (NSNumber *) value;
+            const char *typeCode = n.objCType;
+            if (n == (void*)kCFBooleanFalse || n == (void*)kCFBooleanTrue) {
+                suffix = @"_bool";
+            } else if (!strcmp(typeCode,"f") || !strcmp(typeCode,"d")) {
+                suffix = @"_real";
+            }
+        } else if([value isKindOfClass:[NSString class]]) {
+            suffix = @"_str";
+        } else if ([value isKindOfClass:[NSDate class]]) {
+            suffix = @"_date";
+            NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+            NSLocale *enUSPOSIXLocale = [NSLocale localeWithLocaleIdentifier:@"en_US_POSIX"];
+            [dateFormatter setLocale:enUSPOSIXLocale];
+            [dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss'Z'"];
+            [dateFormatter setCalendar:[NSCalendar calendarWithIdentifier:NSCalendarIdentifierGregorian]];
+            NSDate *date = (NSDate *)value;
+            NSString *iso8601String = [dateFormatter stringFromDate:date];
+            [suffixedProperty setObject:iso8601String forKey:[key stringByAppendingString:suffix]];
+            continue;
+        }
+        [suffixedProperty setObject:value forKey:[key stringByAppendingString:suffix]];
+    }
+    return suffixedProperty;
 }
 
 - (NSString *) getTrimKey:(NSString *) key {
@@ -86,8 +128,20 @@
     [RSLogger logDebug:@"FullStory Factory doesn't support Flush Call"];
 }
 
-BOOL isEmpty(NSString *value) {
-    return value == nil || value.length == 0;
+BOOL isEmpty(NSObject *value) {
+    if (value == nil) {
+        return true;
+    }
+    if ([value isKindOfClass:[NSString class]]) {
+        return [(NSString *)value isEqualToString:@""];
+    }
+    if ([value isKindOfClass:[NSDictionary class]]) {
+        return [(NSDictionary *)value count] == 0;
+    }
+    if ([value isKindOfClass:[NSMutableDictionary class]]) {
+        return [(NSMutableDictionary *)value count] == 0;
+    }
+    return false;
 }
 
 @end
